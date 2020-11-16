e4ed9d75e6248b3ac7d64b537e185cb3126fcef6
farhad nooralahzadeh <farhad.nooralahzadeh@uzh.ch>
2020-11-16 10:05:26 +0100

    vit large withot normalization

--- Model/datasets.py
+++ Model/datasets.py
@@ -43,6 +43,24 @@ TRANSFORM_TEST=transforms.Compose([
 
     ])
 
+train_transforms = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ]
+)
+
+test_transforms = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ]
+)
+
 TRANSFORM_TRAIN=transforms.Compose([
     transforms.RandomResizedCrop(384),
     transforms.RandomHorizontalFlip(),
@@ -59,7 +77,7 @@ class Split(Enum):
 
 def image_to_tensor_ten_crop_auto_augment(img_path, mode=None):
     im_rgb = Image.open(img_path).convert('RGB')
-    img_trs = TRANSFORM_TRAIN(im_rgb) if (mode == Split.train) else TRANSFORM_TEST(im_rgb)
+    img_trs = train_transforms(im_rgb) if (mode == Split.train) else test_transforms(im_rgb)
     #print(img_trs.size())
     return img_trs
 
